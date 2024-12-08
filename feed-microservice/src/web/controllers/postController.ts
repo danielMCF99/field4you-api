e0daf2695cac7f1510cbb1e069c057d72f220189
Novaisn <nelsonnovais@ipvc.pt>
2024-12-08 12:48:52 +0000

    hot fix validate expiration date get 10 posts

--- feed-microservice/src/web/controllers/postController.ts
+++ feed-microservice/src/web/controllers/postController.ts
@@ -1,22 +1,22 @@
-import { Request, Response } from 'express';
-import mongoose from 'mongoose';
-import { Post } from '../../domain/entities/Post';
-import { createPost } from '../../application/use-cases/createPost';
-import { getLast10 } from '../../application/use-cases/getLast10';
-import { deletePost } from '../../application/use-cases/deletePost';
-import { authMiddleware, firebase, jwtHelper, postRepository } from '../../app';
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+import { Post } from "../../domain/entities/Post";
+import { createPost } from "../../application/use-cases/createPost";
+import { getLast10 } from "../../application/use-cases/getLast10";
+import { deletePost } from "../../application/use-cases/deletePost";
+import { authMiddleware, firebase, jwtHelper, postRepository } from "../../app";
 
 export const createPostController = async (req: Request, res: Response) => {
   // Extract token from Authorization Header
   const token = await jwtHelper.extractBearerToken(req);
   if (!token) {
-    res.status(401).json({ message: 'Bearer token required' });
+    res.status(401).json({ message: "Bearer token required" });
     return;
   }
 
   const decodedPayload = await jwtHelper.decodeBearerToken(token);
   if (!decodedPayload) {
-    res.status(401).json({ message: 'Bearer token required' });
+    res.status(401).json({ message: "Bearer token required" });
     return;
   }
 
@@ -24,13 +24,13 @@ export const createPostController = async (req: Request, res: Response) => {
   const tokenExpired = await authMiddleware.validateTokenExpirationDate(exp);
 
   if (!tokenExpired) {
-    res.status(401).json({ message: 'Bearer token validation expired' });
+    res.status(401).json({ message: "Bearer token validation expired" });
     return;
   }
 
   if (!req.file) {
     res.status(400).json({
-      message: 'Please upload an Image',
+      message: "Please upload an Image",
     });
     return;
   }
@@ -38,7 +38,7 @@ export const createPostController = async (req: Request, res: Response) => {
   const publicUrl = await firebase.uploadFileToFirebase(req);
   if (!publicUrl) {
     res.status(500).json({
-      message: 'Something went wrong storing image',
+      message: "Something went wrong storing image",
     });
     return;
   }
@@ -55,19 +55,19 @@ export const createPostController = async (req: Request, res: Response) => {
     const newPost = await createPost(postRequest, postRepository);
     if (!newPost) {
       res.status(500).json({
-        message: 'Something went wrong for new post creation',
+        message: "Something went wrong for new post creation",
       });
       return;
     }
 
     res.status(200).json({
-      message: 'Successfully created the post.',
+      message: "Successfully created the post.",
     });
     return;
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: 'Something went wrong',
+      message: "Something went wrong",
     });
     return;
   }
@@ -76,21 +76,21 @@ export const createPostController = async (req: Request, res: Response) => {
 export const getLast10PostController = async (req: Request, res: Response) => {
   const token = await jwtHelper.extractBearerToken(req);
   if (!token) {
-    res.status(401).json({ message: 'Bearer token required' });
+    res.status(401).json({ message: "Bearer token required" });
     return;
   }
 
   const decodedPayload = await jwtHelper.decodeBearerToken(token);
   if (!decodedPayload) {
-    res.status(401).json({ message: 'Bearer token required' });
+    res.status(401).json({ message: "Bearer token required" });
     return;
   }
-
+  console.log(decodedPayload);
   const { exp } = decodedPayload;
   const tokenExpired = await authMiddleware.validateTokenExpirationDate(exp);
 
-  if (tokenExpired) {
-    res.status(401).json({ message: 'Bearer token validation expired' });
+  if (!tokenExpired) {
+    res.status(401).json({ message: "Bearer token validation expired" });
     return;
   }
 
@@ -101,7 +101,7 @@ export const getLast10PostController = async (req: Request, res: Response) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: 'Something went wrong',
+      message: "Something went wrong",
     });
     return;
   }
@@ -112,12 +112,12 @@ export const deletePostController = async (req: Request, res: Response) => {
   const token = await jwtHelper.extractBearerToken(req);
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    res.status(400).json({ message: 'Invalid ID format' });
+    res.status(400).json({ message: "Invalid ID format" });
     return;
   }
 
   if (!token) {
-    res.status(401).json({ message: 'Bearer token required' });
+    res.status(401).json({ message: "Bearer token required" });
     return;
   }
 
@@ -127,7 +127,7 @@ export const deletePostController = async (req: Request, res: Response) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: 'Something went wrong',
+      message: "Something went wrong",
     });
     return;
   }
