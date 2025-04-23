53f4bba2621a2fd332a44de0d106dc7792553689
danielMCF99 <carvalhodani99@hotmail.com>
2025-04-23 02:49:40 +0100

    Add mechanism to cancel bookings when sports venue is deleted

--- booking-microservice/src/application/use-cases/sportsVenue/deleteSportsVenue.ts
+++ booking-microservice/src/application/use-cases/sportsVenue/deleteSportsVenue.ts
@@ -1,10 +1,47 @@
 import { SportsVenue } from '../../../domain/entities/SportsVenue';
-import { sportsVenueRepository } from '../../../app';
+import {
+  bookingInviteRepository,
+  bookingRepository,
+  sportsVenueRepository,
+} from '../../../app';
+import mongoose from 'mongoose';
+import { BookingInviteStatus } from '../../../domain/entities/BookingInvite';
 
 export const deleteSportsVenue = async (
   id: string
 ): Promise<SportsVenue | null> => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
   try {
+    // Find all active pending bookings for sportsVenues
+    const activeBookings = await bookingRepository.findAllActiveByVenueIds([
+      id,
+    ]);
+
+    const bookingIds = activeBookings.map((b) => b.getId());
+
+    // Reject invites associated to bookings
+    const updatedInvitesByBookingIds =
+      await bookingInviteRepository.bulkUpdateStatusByBookingIds(
+        bookingIds,
+        BookingInviteStatus.rejected,
+        'Invites rejected due to sports venue deletion',
+        session
+      );
+
+    // Cancel bookings
+    const canceledBookingsByIds = await bookingRepository.bulkStatusUpdateByIds(
+      bookingIds,
+      session
+    );
+
+    console.log(
+      `${updatedInvitesByBookingIds.modifiedCount} invites updated by booking ids.`
+    );
+    console.log(
+      `${canceledBookingsByIds.modifiedCount} canceled bookings by id.`
+    );
+
     return await sportsVenueRepository.delete(id);
   } catch (error) {
     console.error(error);
