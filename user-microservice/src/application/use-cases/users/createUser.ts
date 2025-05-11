daf1a921208232a76730ed86bfd0332c141427bc
Novaisn <nelsonnovais@ipvc.pt>
2025-05-11 17:45:47 +0100

    Nominatim API request to retrieve coordinates via longitude and latitude

--- user-microservice/src/application/use-cases/users/createUser.ts
+++ user-microservice/src/application/use-cases/users/createUser.ts
@@ -1,9 +1,21 @@
 import { userRepository } from '../../../app';
 import { User } from '../../../domain/entities/User';
 import { InternalServerErrorException } from '../../../domain/exceptions/InternalServerErrorException';
+import { getCoordinatesFromAddress } from '../../../infrastructure/utils/getCoordinatesFromAddress';
 
 export const createUser = async (user: any): Promise<User | undefined> => {
   try {
+    const { address, city, district } = user.location;
+
+    try {
+      const coords = await getCoordinatesFromAddress(address, city, district);
+      user.location.latitude = coords.latitude;
+      user.location.longitude = coords.longitude;
+    } catch (err) {
+      console.log(err);
+      console.log('Could not fetch coordinates:');
+    }
+
     const userId = user.userId;
     user._id = userId.toString();
     const newUser = await userRepository.create(user);
