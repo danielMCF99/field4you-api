3e9e42bafed6f5a08bad0add22ac94a19bad60a2
Novaisn <nelsonnovais@ipvc.pt>
2025-05-31 23:01:08 +0100

    hot fix user url in post service

--- feed-microservice/src/post/post.service.ts
+++ feed-microservice/src/post/post.service.ts
@@ -44,7 +44,7 @@ export class PostService {
     let profileImageUrl = 'DefaultProfileImage';
     try {
       const userRes = await firstValueFrom(
-        this.httpService.get(`${userServiceUrl}/${creatorId}`),
+        this.httpService.get(`${userServiceUrl}${creatorId}`),
       );
       profileImageUrl = userRes.data.imageURL || profileImageUrl;
     } catch (err) {
@@ -52,7 +52,6 @@ export class PostService {
         'Não foi possível obter o utilizador, a usar imagem default',
       );
     }
-    console.log(profileImageUrl);
     const post = await this.postModel.create({
       creatorId,
       creatorEmail,
@@ -69,7 +68,15 @@ export class PostService {
 
   async listPosts(filters: GetAllPostsDto) {
     console.log('Entered get all posts service');
-    const { creatorEmail, profileImageUrl, startDate, endDate, page, limit, userType } = filters;
+    const {
+      creatorEmail,
+      profileImageUrl,
+      startDate,
+      endDate,
+      page,
+      limit,
+      userType,
+    } = filters;
 
     const query: any = {};
 
@@ -96,7 +103,9 @@ export class PostService {
     const [postsList, totalPosts] = await Promise.all([
       this.postModel
         .find(query)
-        .select('_id creatorEmail comment profileImageUrl fileName imageUrl createdAt')
+        .select(
+          '_id creatorEmail comment profileImageUrl fileName imageUrl createdAt',
+        )
         .sort({ createdAt: -1 })
         .skip(skip)
         .limit(limit)
