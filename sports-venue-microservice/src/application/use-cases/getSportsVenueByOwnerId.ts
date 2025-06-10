3274ace9d415ec7017f39904e952219dbdbf1f1b
AAVANDRE10 <andrevale_6@hotmail.com>
2025-06-10 14:17:41 +0100

    add query params getSportsVenueByOwnerId

--- sports-venue-microservice/src/application/use-cases/getSportsVenueByOwnerId.ts
+++ sports-venue-microservice/src/application/use-cases/getSportsVenueByOwnerId.ts
@@ -1,20 +1,34 @@
 import { Request } from 'express';
 import { sportsVenueRepository } from '../../app';
 import { SportsVenue } from '../../domain/entities/SportsVenue';
-import { NotFoundException } from '../../domain/exceptions/NotFoundException';
+import { SportsVenueFilterParams } from '../../domain/dtos/sports-venue-filter.dto';
 import mongoose from 'mongoose';
 import { InternalServerErrorException } from '../../domain/exceptions/InternalServerErrorException';
 
 export const getSportsVenueByOwnerId = async (
+  query: any,
   req: Request
-): Promise<SportsVenue []> => {
-
+): Promise<{ totalPages: number; sportsVenues: SportsVenue[] }> => {
   const userId = req.headers['x-user-id'] as string | undefined;
 
   if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
-    throw new InternalServerErrorException('Internal Server Error. user id header missing.');
+    throw new InternalServerErrorException('Invalid or missing user ID');
   }
-  const sportsVenue = await sportsVenueRepository.findByOwnerId(userId);
+
+  const filters: SportsVenueFilterParams = {
+    sportsVenueName: query.sportsVenueName?.toString(),
+    status: query.status?.toString(),
+    page: query.page ? parseInt(query.page.toString(), 10) : 1,
+    limit: query.limit ? parseInt(query.limit.toString(), 10) : 10,
+    district: query.district?.toString(),
+  };
+
+  // Usar el repositorio con los filtros
+  const result = await sportsVenueRepository.findByOwnerId(userId, filters);
   
-  return sportsVenue;
+  if (!result) {
+    throw new InternalServerErrorException('Failed to retrieve sports venues');
+  }
+
+  return result;
 };
